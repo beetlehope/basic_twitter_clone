205af09dbbbec1b5038a8ce75eb299b442f9b509
beetlehope <beetlehope@gmail.com>
2017-07-23 16:43:46 +0300

    Implement advanced login

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -67,6 +67,10 @@ class UserTest < ActiveSupport::TestCase
      assert_not @user.valid?
    end
 
+   test "authenticated? should return false for a user with nil digest" do
+    assert_not @user.authenticated?('')
+  end
+
 
 
 
