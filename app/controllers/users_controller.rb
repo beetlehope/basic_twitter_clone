afc9979f62b3bc1586c97e98637d33316ab40440
beetlehope <beetlehope@gmail.com>
2017-07-27 14:34:56 +0300

    Add user microposts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,6 +9,7 @@ before_action :admin_user,     only: :destroy
 
   def show
     @user = User.find(params[:id])
+    @microposts = @user.microposts.paginate(page: params[:page])
   end
 
   def new
@@ -53,13 +54,6 @@ before_action :admin_user,     only: :destroy
                                      :password_confirmation)
       end
 
-      def logged_in_user
-        unless logged_in?
-          store_location
-          flash[:danger] = "Please log in."
-          redirect_to login_url
-      end
-    end
 
     def correct_user
       @user = User.find(params[:id])
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  has_many :microposts, dependent: :destroy
   attr_accessor :remember_token, :activation_token, :reset_token
   before_save   :downcase_email
   before_create :create_activation_digest
@@ -64,7 +65,11 @@ class User < ApplicationRecord
  # Returns true if a password reset had expired
  def password_reset_expired?
     reset_sent_at < 2.hours.ago
-  end
+ end
+
+ def feed
+   Micropost.where("user_id = ?", id)
+ end
 
 
   private
