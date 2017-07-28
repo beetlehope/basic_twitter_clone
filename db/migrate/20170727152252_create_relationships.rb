6790ee509f896722bffb2920b750227f37aa6017
beetlehope <beetlehope@gmail.com>
2017-07-28 16:24:33 +0300

    Add user following

--- db/migrate/20170727152252_create_relationships.rb
+++ db/migrate/20170727152252_create_relationships.rb
@@ -1,4 +1,4 @@
-class CreateRelationships < ActiveRecord::Migration[5.0]
+
   class CreateRelationships < ActiveRecord::Migration[5.0]
    def change
      create_table :relationships do |t|
@@ -12,4 +12,3 @@ class CreateRelationships < ActiveRecord::Migration[5.0]
      add_index :relationships, [:follower_id, :followed_id], unique: true
    end
  end
-end
